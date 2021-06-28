82e3dbc3733943bedf378813c2c962b048f09db0
Arthur <arthurfmg@hotmail.com>
2021-06-28 10:38:48 -0300

    commit backup

---
+++ Assets/Scripts/BolaDeFogo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BolaDeFogo : MonoBehaviour {
+	private float posicaoY = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject,2f);
+		GetComponent<Rigidbody2D>().AddForce(transform.up * 400f);
+		posicaoY = transform.position.y;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (posicaoY > transform.position.y) { //Descendo
+			transform.eulerAngles = new Vector2(180, 0);
+		}
+		posicaoY = transform.position.y;
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float velocidade;
+	public float forcaPulo;
+	private bool estaNoChao;
+	public Transform chaoVerificador;
+	public Transform spritePlayer;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = spritePlayer.GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movimentacao();
+	}
+
+	void Movimentacao() {
+		if (Input.GetAxisRaw("Horizontal") > 0 ) {
+			transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 0);
+		} 	
+
+		if (Input.GetAxisRaw("Horizontal") < 0 ) {
+			transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+
+		if (Input.GetButtonDown("Jump") && estaNoChao) {
+			GetComponent<Rigidbody2D>().AddForce(transform.up * forcaPulo);
+		}
+
+		estaNoChao = Physics2D.Linecast(transform.position, chaoVerificador.position, 1 << LayerMask.NameToLayer("Piso"));
+		animator.SetBool ("Chao", estaNoChao);
+
+		animator.SetFloat("Movimento", Mathf.Abs (Input.GetAxisRaw ("Horizontal")));
+
+	}
+}
